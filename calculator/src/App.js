c866a51bec5cad69fa72df6547af9ea740930de3
amisha patel <ami.patel2410@gmail.com>
2018-01-31 11:16:55 -0500

    final commit

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
       total: 0
     };
     this.add1 = this.add1.bind(this);
+    this.add5 = this.add5.bind(this);
+    this.sub1 = this.sub1.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   add1()
@@ -18,11 +21,35 @@ class App extends Component {
     });
   }
 
+  add5()
+  {
+    this.setState({
+      total: this.state.total+5
+    });
+  }
+
+  sub1()
+  {
+    this.setState({
+      total: this.state.total-1
+    });
+  }
+
+  reset()
+  {
+    this.setState({
+      total: 0
+    });
+  }
+
   render() {
     return (
       <div>
       <h1> {this.state.total} </h1>
         <button onClick={this.add1}> Add 1 </button>
+        <button onClick={this.add5}> Add 5 </button>
+        <button onClick={this.sub1}> Sub -1 </button>
+        <button onClick={this.reset}> Reset </button>
       </div>
     );
   }
